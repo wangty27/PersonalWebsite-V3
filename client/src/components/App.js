3a01fb9a56a7bc9eac54968b11625ae310e945ca
Tianyu Wang <terrywang@tianyu.ninja>
2019-10-10 15:52:35 -0400

    root component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Scroll from 'react-scroll';
 
-class App extends React.Component {
-  com
+import NavBar from './NavBar';
 
-  render() {
-    return (
-      <div>Front-end</div>
-    );
-  }
+var Element = Scroll.Element;
+
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<NavBar />
+				<Element name='home' style={{ height: '100vh', background: 'black', paddingTop: '100px' }}>
+					<h1 style={{ color: 'white' }}>Home</h1>
+				</Element>
+				<Element name='about' style={{ height: '100vh', background: 'black', paddingTop: '100px' }}>
+					<h1 style={{ color: 'white' }}>about</h1>
+				</Element>
+				<Element name='experience' style={{ height: '100vh', background: 'black', paddingTop: '100px' }}>
+					<h1 style={{ color: 'white' }}>experience</h1>
+				</Element>
+				<Element name='projects' style={{ height: '100vh', background: 'black', paddingTop: '100px' }}>
+					<h1 style={{ color: 'white' }}>projects</h1>
+				</Element>
+				<Element name='contact' style={{ height: '100vh', background: 'black', paddingTop: '100px' }}>
+					<h1 style={{ color: 'white' }}>contact</h1>
+				</Element>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
