38f030366e4f1212fb53eaf0b27b66117b749d06
Tianyu Wang <terrywang@tianyu.ninja>
2019-10-29 01:09:02 -0400

    Serve front-end with express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 dotenv.config();
 const app = express();
 
 app.use('/static', express.static('static'));
+
+var FRONT_END_PATH: string = path.resolve(__dirname, 'client', 'build');
+if (process.env.NODE_ENV !== 'production') {
+	FRONT_END_PATH = path.resolve(__dirname, '..', 'client', 'build');
+}
+
+app.use(express.static(FRONT_END_PATH));
+app.get('*', (req, res) => {
+	res.sendFile(FRONT_END_PATH + 'index.html');
+});
 
 const PORT = process.env.PORT || '8080';
 
