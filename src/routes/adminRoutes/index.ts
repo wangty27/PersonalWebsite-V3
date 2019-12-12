2753cc50dbb6b7266b9b3a34dce93ef0bb1d9164
Tianyu Wang <terrywang@tianyu.ninja>
2019-12-12 18:20:03 -0500

    Minor fix

--- src/routes/adminRoutes/index.ts
+++ src/routes/adminRoutes/index.ts
@@ -1,12 +1,14 @@
 import * as fs from 'fs';
 import { join } from 'path';
 
-const homeDataFileDir: string = join(__dirname, '..', '..', 'data');
+const dataFileDir: string = join(__dirname, '..', '..', 'data');
 const homeDataFilePath: string = join(__dirname, '..', '..', 'data', 'home.json');
+const aboutDataFilePath: string = join(__dirname, '..', '..', 'data', 'about.json');
+const experienceDataFilePath: string = join(__dirname, '..', '..', 'data', 'experience.json');
 
-fs.access(homeDataFileDir, fs.constants.F_OK, err => {
+fs.access(dataFileDir, fs.constants.F_OK, err => {
 	if (err) {
-		fs.mkdir(homeDataFileDir, { recursive: true }, err => {
+		fs.mkdir(dataFileDir, { recursive: true }, err => {
 			if (err) {
 				throw err;
 			}
@@ -17,6 +19,33 @@ fs.access(homeDataFileDir, fs.constants.F_OK, err => {
 fs.access(homeDataFilePath, fs.constants.F_OK, err => {
 	if (err) {
 		fs.writeFile(homeDataFilePath, '[]', err => {
+			if (err) {
+				throw err;
+			}
+		});
+	}
+});
+
+fs.access(aboutDataFilePath, fs.constants.F_OK, err => {
+	if (err) {
+		var initValue = {
+			description: '',
+			keywords: [],
+			skills: [],
+			resume: '',
+			portrait: ''
+		};
+		fs.writeFile(aboutDataFilePath, initValue, err => {
+			if (err) {
+				throw err;
+			}
+		});
+	}
+});
+
+fs.access(experienceDataFilePath, fs.constants.F_OK, err => {
+	if (err) {
+		fs.writeFile(experienceDataFilePath, '[]', err => {
 			if (err) {
 				throw err;
 			}
