dc61c9863822b096d4c9ba00e457fec984636612
Tianyu Wang <terrywang@tianyu.ninja>
2019-12-16 00:48:17 -0500

    Added router to handle admin and 404 routes

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
-import Scroll from 'react-scroll';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import NavBar from './NavBar';
-import ToTop from './ToTop';
-import Gap from './Gap';
-import Home from './Home';
-import About from './about/About';
-import Experience from './experience/Experience';
+import Main from './Main';
 
-var Element = Scroll.Element;
+const Admin = () => <div>Admin</div>;
+const NotFound404 = () => <div>404</div>;
 
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<NavBar />
-				<Element id='home' name='home'>
-					<Home />
-				</Element>
-				<Element id='about' name='about'>
-					<About />
-				</Element>
-				<Gap number={1} />
-				<Element id='experience' name='experience'>
-					<Experience />
-				</Element>
-				<Gap number={2} />
-				<Element id='projects' name='projects' style={{ height: '100vh', paddingTop: '100px' }}>
-					<h1 style={{ color: 'white' }}>projects</h1>
-				</Element>
-				<Element id='contact' name='contact' style={{ height: '100vh', paddingTop: '100px' }}>
-					<h1 style={{ color: 'white' }}>contact</h1>
-				</Element>
-				<ToTop />
+				<BrowserRouter>
+					<Switch>
+						<Route exact path='/admin' component={Admin} />
+						<Route exact path='/' component={Main} />
+						<Route component={NotFound404} />
+					</Switch>
+				</BrowserRouter>
 			</React.Fragment>
 		);
 	}
